9a23b468d0d3147d06435d98fa1a4537d4fb2df8
Arjun Jagdish Ram <arjunjram@gmail.com>
2018-03-19 18:36:33 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='KalmanFilter',
+      version='1.0',
+      description='Kalman Filters and State Estimation Modules',
+      url='https://github.com/MapleNSteel/Kalman-Filter',
+      author='MapleNSteel',
+      author_email='arjunjram@gmail.com',
+      license='MIT',
+      packages=['KalmanFilters','StateEstimationModules'],
+      zip_safe=False)
